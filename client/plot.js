a28db5427f644f077f3cce10fe7a406cfe73b498
Guido <me@guido.io>
2015-06-05 23:02:18 +0200

    Plottas

---
+++ client/plot.js
@@ -0,0 +1,34 @@
+
+Template.plot.onRendered (function(a) {
+
+  var values = (this.data ? this.data.values: []) ? this.data.values: [];
+  var plotElement = this.find('.plot');
+
+  var plotData = {key: this.data.title, values: values};
+  var xAxisName = this.data.xAxis;
+  var yAxisName = this.data.yAxis;
+
+  nv.addGraph(function() {
+    var chart = nv.models.lineWithFocusChart();
+
+    chart.xAxis
+        .tickFormat(d3.format(',f'))
+        .axisLabel(xAxisName);
+
+    chart.yAxis
+        .tickFormat(d3.format(',.2f'))
+        .axisLabel(yAxisName);
+
+    chart.y2Axis
+        .tickFormat(d3.format(',.2f'));
+
+    d3.select(plotElement)
+        .datum([plotData])
+        .transition().duration(500)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+});
